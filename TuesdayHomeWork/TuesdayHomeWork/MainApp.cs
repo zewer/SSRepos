bcba14febc87a26ee548ec23b6c58f709e796106
Kutaev Serhiy <zewer1@yandex.ru>
2017-12-18 23:01:45 +0200

    HomeWork
    Some changes

--- TuesdayHomeWork/TuesdayHomeWork/MainApp.cs
+++ TuesdayHomeWork/TuesdayHomeWork/MainApp.cs
@@ -1,16 +1,17 @@
 ﻿namespace TuesdayHomeWork
 {
     public delegate void GetTaskFunc();
-
+    
     public class MainApp
-    {
+    {        
         public static void Main(string[] args)
         {
-            /*1. Define integer variables a and b.Read values a and b from Console and calculate: a + b, a - b, a* b, a/ b.Output obtained results.
-              2. Output question “How are you ?“. Define string variable answer.Read the value answer and output: “You are (answer)".
-              3. Read 3 variables of char type. Write message: “You enter (first char), (second char), (3 char)”
-              4. Enter 2 integer numbers. Check if they are both positive – use bool expretion
-            /*/
+            /*
+            1. Define integer variables a and b.Read values a and b from Console and calculate: a + b, a - b, a* b, a/ b.Output obtained results.
+            2. Output question “How are you ?“. Define string variable answer.Read the value answer and output: “You are (answer)".
+            3. Read 3 variables of char type. Write message: “You enter (first char), (second char), (3 char)”
+            4. Enter 2 integer numbers. Check if they are both positive – use bool expretion
+            */
             GetTaskFunc deleg;
             deleg = HomeTask.GetTask1;
             deleg += HomeTask.GetTask2;
